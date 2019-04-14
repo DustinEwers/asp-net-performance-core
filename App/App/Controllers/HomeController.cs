a01f5fda96fcf11fd5466b20729cf9c3e7c08c8b
Dustin Ewers <dustin.ewers@gmail.com>
2019-04-14 18:33:47 -0500

    demo updates

--- App/App/Controllers/HomeController.cs
+++ App/App/Controllers/HomeController.cs
@@ -1,17 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using App.Models;
 using System.Threading;
 using StackExchange.Profiling;
+using App.Services;
 
 namespace App.Controllers
 {
     public class HomeController : Controller
     {
+        public ICustomerService service;
+
+        public HomeController(ICustomerService _service) {
+            service = _service;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -19,7 +24,7 @@ namespace App.Controllers
 
         public IActionResult About()
         {
-            var profiler = MiniProfiler.StartNew("Profiler on the About Controller");
+            var profiler = MiniProfiler.StartNew("Profiler on the Home Controller");
             using (profiler.Step("Some Work")) {
                 Thread.Sleep(100);
                 ViewData["Message"] = "Your application description page.";
@@ -30,7 +35,11 @@ namespace App.Controllers
         [ResponseCache(CacheProfileName = "Default")]
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            using (MiniProfiler.Current.Step("Some Work"))
+            {
+                Thread.Sleep(100);
+                ViewData["Message"] = "Your contact page.";
+            }
 
             return View();
         }
@@ -39,6 +48,37 @@ namespace App.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+
+        private async Task<List<Customer>> GetCustomerDataAsync() {
+            var customers = await service.GetCustomersAsync();
+            var details = await service.GetExtraCustomerDataFromService();
+            var moreDetails = await service.GetMoreExternalCustomerDataFromService();
+
+            return AssembleCustomerDetails(details, moreDetails, customers);
+        }
+
+        private List<Customer> GetCustomerDataParallel()
+        {
+            // Kick off all of these in parallel
+            var customerTask = service.GetCustomersAsync();
+            var detailsTask = service.GetExtraCustomerDataFromService();
+            var moreDetailsTask = service.GetMoreExternalCustomerDataFromService();
+
+            // Wait until all of the results come back
+            Task.WaitAll(customerTask, detailsTask, moreDetailsTask);
+
+            return AssembleCustomerDetails(detailsTask.Result, 
+                moreDetailsTask.Result, 
+                customerTask.Result);
+        }
+
+
+
+
+        private List<Customer> AssembleCustomerDetails(List<string> thing1, List<string> thing2, List<Customer> customers) {
+            return customers;
         }
     }
 }
---
+++ App/App/Services/CustomerService.cs
@@ -0,0 +1,42 @@
+﻿using App.DataAccess;
+using App.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace App.Services
+{
+    public interface ICustomerService {
+        Task<List<string>> GetExtraCustomerDataFromService();
+        Task<List<string>> GetMoreExternalCustomerDataFromService();
+        Task<List<Customer>> GetCustomersAsync();
+    }
+
+    public class CustomerService : ICustomerService
+    {
+        private readonly AppDbContext _context;
+
+        public async Task<List<Customer>> GetCustomersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<string>> GetExtraCustomerDataFromService()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<string>> GetMoreExternalCustomerDataFromService()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Customer>> GetCustomers() {
+            return await _context.Customers
+                .Include(x => x.Orders).ThenInclude(x => x.OrderLines)
+                .ToListAsync();
+        }
+    }
+}
